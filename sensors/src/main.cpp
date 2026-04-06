8236a9ed864c79ea680909c25f00adaa49d7560f
brzozio <michal.brzozowski@interia.eu>
2026-04-06 14:45:29 +0200

    Modified code for DHT11 sensor and GPIO pin numbering

--- sensors/src/main.cpp
+++ sensors/src/main.cpp
@@ -1,21 +1,20 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <DHT.h>
 
 // ==== CONFIG ====
-const char* WIFI_SSID = "YOUR_WIFI";
-const char* WIFI_PASSWORD = "YOUR_PASSWORD";
+const char* WIFI_SSID = "";
+const char* WIFI_PASSWORD = "";
 
 const char* MQTT_SERVER = "192.168.1.100"; // your broker IP
 const int MQTT_PORT = 1883;
 const char* MQTT_TOPIC = "esp32/temperature";
 
-// ==== DS18B20 ====
-#define ONE_WIRE_BUS 4
+// ==== DHT11 ====
+#define DHTPIN 8
+#define DHTTYPE DHT11
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+DHT dht(DHTPIN, DHTTYPE);
 
 // ==== MQTT ====
 WiFiClient espClient;
@@ -24,11 +23,16 @@ PubSubClient client(espClient);
 // ==== FUNCTIONS ====
 
 void connectWiFi() {
+    Serial.println("WiFi start");
+
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     while (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Connecting WiFi...");
         delay(500);
     }
+
+    Serial.println("WiFi OK");
 }
 
 void connectMQTT() {
@@ -45,12 +49,15 @@ void connectMQTT() {
 
 void setup() {
     Serial.begin(115200);
+    delay(2000);
+
+    Serial.println("BOOT OK");
+
+    dht.begin();
 
     connectWiFi();
-
     client.setServer(MQTT_SERVER, MQTT_PORT);
 
-    sensors.begin();
 }
 
 void loop() {
@@ -60,20 +67,22 @@ void loop() {
 
     client.loop();
 
-    sensors.requestTemperatures();
-    float tempC = sensors.getTempCByIndex(0);
+    float tempC = dht.readTemperature();
+    float humidity = dht.readHumidity();
 
-    if (tempC != DEVICE_DISCONNECTED_C) {
-        char payload[10];
-        dtostrf(tempC, 1, 2, payload);
+    if (!isnan(tempC) && !isnan(humidity)) {
+        char payload[50];
+        sprintf(payload, "{\"temp\":%.2f,\"hum\":%.2f}", tempC, humidity);
 
         client.publish(MQTT_TOPIC, payload);
 
-        Serial.print("Temperature: ");
-        Serial.println(payload);
+        Serial.print("Temp: ");
+        Serial.print(tempC);
+        Serial.print(" °C | Hum: ");
+        Serial.println(humidity);
     } else {
-        Serial.println("Sensor error");
+        Serial.println("DHT read error");
     }
 
-    delay(5000);
+    delay(2000);
 }
